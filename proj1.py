12a34b8cc2fc45dd9dcc4980628784707de8ae43
Steve Sibi <steve.sibi@gmail.com>
2020-03-06 16:47:10 +0400

    GUI updated. Currently only accepts text/ciphertext from variable inside python. Otherwise everything else works.

--- proj1.py
+++ proj1.py
@@ -3,15 +3,82 @@ from string import ascii_lowercase
 from string import punctuation
 import collections
 
+##########################################################################
+# start of GUI
+
+root = Tk()  # root window
+root.title("Project 1 - Cryptography")  # title for GUI window
+
+width, height = root.winfo_screenwidth(), root.winfo_screenheight()
+root.geometry('%dx%d+0+0' % (width, height))
+# root.geometry("1350x750")
+root.config(bg="grey")  # background color
+
+titleFrame = Frame(root)
+titleFrame.pack(side=TOP, pady=10)
+
+topFrame = Frame(root)
+topFrame.pack(pady=10)
+
+bottomFrame = Frame(root)
+bottomFrame.pack(padx=10, pady=10)
+
+bottomFrame0 = Frame(bottomFrame)
+bottomFrame0.pack(side=LEFT, padx=10, pady=20)
+
+bottomFrame1 = Frame(bottomFrame)
+bottomFrame1.pack(side=LEFT, padx=10)
+
+button_frame = Frame(bottomFrame0)
+button_frame.pack(side=BOTTOM)
+
+button_frame1 = Frame(root, bg="grey")
+button_frame1.pack()
+
+title_label = Label(titleFrame, text="Project 1 GUI")
+title_label.pack(fill=X, pady=10)
+
+key_label = Label(topFrame, text="Key:")
+key_label.pack(side=LEFT, fill=Y, pady=5)
+
+key_input = Entry(topFrame, width=60)
+key_input.pack(side=LEFT, fill=Y, pady=10, padx=20)
+
+def retrieve_key():
+    monoKey = key_input.get()
+    return monoKey
+
+key_button = Button(topFrame, text="select", command=retrieve_key)
+key_button.pack(side=LEFT, fill=Y, padx=5, pady=5)
+
+plaintext_label = Label(bottomFrame0, text="Enter Text")
+plaintext_label.pack(pady=10)
+
+plaintext_entry = Text(bottomFrame0, height=28)
+plaintext_entry.pack(padx=10)
+
+plaintext_label1 = Label(bottomFrame1, text="Result")
+plaintext_label1.pack(pady=5)
+
+plaintext_entry1 = Text(bottomFrame1, height=30)
+plaintext_entry1.pack(padx=10, pady=10)
+
+# def save_input():
+#     text = plaintext_entry.get(1.0, END)
+#     text = text.lower()
+#     print(text)
+#     return text
 
 alphabets = ''.join(list(ascii_lowercase))
-monoKey = "hljfrtzyqscnbexvumpaokwdgi"  # should be input
+# monoKey = "hljfrtzyqscnbexvumpaokwdgi"  # should be input
+
 length = 26
 # text="ya test test hi test test hi test test hi test test"
 text = "one way to solve an encrypted message if we know its language is to find a different plaintext of the same language long enough to fill one sheet or so and then we count the occurrences of each letter we call the most frequently occurring letter the first the next most occurring letter the second the following most occurring letter the third and so on until we account for all the different letters in the plaintext sample then we look at the cipher text we want to solve and we also classify its symbols we find the most occurring symbol and change it to the form of the first letter of the plaintext sample the next most common symbol is changed to the form of the second letter and the following most common symbol is changed to the form of the third letter and so on until we account for all symbols of the cryptogram we want to solve"
 # text="the european languages are members of the same family their separate existence is a myth for science music sport etc europe uses the same vocabulary the languages only differ in their grammar their pronunciation and their most common words everyone realizes why a new common language would be desirable one could refuse to pay expensive translators to achieve this it would be necessary to have uniform grammar pronunciation and more common words if several languages coalesce the grammar of the resulting language is more simple and regular than that of the individual languages the new common language will be more simple and regular than the existing european languages it will be as simple as occidental in fact it will be occidental to an english person it will seem like simplified english as a skeptical cambridge friend of mine told me what occidental is the european languages are members of the same family their separate existence is a myth for science music sport etc europe uses the same vocabulary the languages only differ in their grammar their pronunciation and their most common words everyone realizes why a new common language would be desirable one could refuse to pay expensive translators to achieve this it would be necessary to have uniform grammar pronunciation and more common words if several languages coalesce the grammar of the resulting language is more simple and regular than that of the individual languages the new common language will be more simple and regular than the existing european languages it will be as simple as occidental in fact it will be occidental to an english person it will seem like simplified english as a skeptical cambridge friend of mine told me what occidental is the european languages are members of the same family their separate existence is a myth for science music sport etc europe uses the same vocabulary the languages only differ in their grammar their pronunciation and their most common words"
 
 text = text.lower()
+# print(text)
 
 no_punc = ""
 for char in text:
@@ -19,7 +86,6 @@ for char in text:
         no_punc += char
 
 toEncrypt = no_punc
-
 
 # This function takes in two parameters and returns two values
 #     parameter 1 -> Character to perform monoalphabetic substitution
@@ -33,13 +99,13 @@ def monoAlphabeticSubstitution(charToSubstitute, action):
         indexAlphabets = alphabets.index(charToSubstitute)
 
         # Encrypting the character based on monoalphabetic substitution key
-        indexMonoKey = monoKey.index(charToSubstitute)
-        charMonoKey = monoKey[(indexMonoKey + 1) % length]
+        indexMonoKey = retrieve_key().index(charToSubstitute)
+        charMonoKey = retrieve_key()[(indexMonoKey + 1) % length]
     else:
 
         # Decrypting character based on monoalphabtic substituion key
-        indexMonoKey = monoKey.index(charToSubstitute)
-        charMonoKey = monoKey[(indexMonoKey - 1) % length]
+        indexMonoKey = retrieve_key().index(charToSubstitute)
+        charMonoKey = retrieve_key()[(indexMonoKey - 1) % length]
 
         # Finding the index of the decrypted character in the alphabets
         indexAlphabets = alphabets.index(charMonoKey)
@@ -96,9 +162,7 @@ def encryptionFunc():
     # print(encrypted)
     return encrypted
 
-
 def decryptionFunc():
-
     toDecrypt = encryptionFunc()
     i = 0
     decrypted = ""
@@ -131,67 +195,31 @@ def decryptionFunc():
     # print(decrypted)
     return decrypted
 
-# print(encryptionFunc())
-# print(decryptionFunc())
-
 def display_encrypt():
-    plaintext_entry.insert("1.0", encryptionFunc())
+    plaintext_entry1.insert("1.0", encryptionFunc())
 
 def display_decrypt():
-    plaintext_entry1.insert("2.0", decryptionFunc())
-##########################################################################
-# start of GUI
-root = Tk()  # root window
-root.title("Project 1 - Cryptography")  # title for GUI window
+    plaintext_entry1.insert("1.0", decryptionFunc())
 
-root.geometry("1350x750")
-root.config(bg="grey")  # background color
+def clear_input():
+    plaintext_entry.delete(1.0, END)
+    plaintext_entry1.delete(1.0, END)
 
-titleFrame = Frame(root)
-titleFrame.pack(side=TOP, pady=30)
+# save_button = Button(button_frame, text="Save")
+# save_button.pack(side=RIGHT)
 
-topFrame = Frame(root)
-topFrame.pack(pady=10)
+encrypt_button = Button(button_frame, text="Encrypt", command=display_encrypt)
+encrypt_button.pack(side=RIGHT, padx=5, pady=5)
 
-bottomFrame = Frame(root)
-bottomFrame.pack(side=LEFT, padx=15)
+decrypt_button = Button(button_frame, text="Decrypt", command=display_decrypt)
+decrypt_button.pack(side=LEFT, pady=10)
 
-bottomFrame1 = Frame(root)
-bottomFrame1.pack(side=LEFT, padx=10)
-
-title_label = Label(titleFrame, text="Project 1 GUI")
-title_label.pack(fill=X, pady=10)
-
-key_label = Label(topFrame, text="Key:")
-key_label.pack(side=LEFT, fill=Y, pady=10)
-
-key_input = Entry(topFrame)
-key_input.pack(side=LEFT, fill=Y, pady=10)
-
-key_button = Button(topFrame, text="select")
-key_button.pack(side=LEFT, fill=Y)
-
-plaintext_label = Label(bottomFrame, text="Plaintext > Encypted")
-plaintext_label.pack(pady=10)
-
-plaintext_entry = Text(bottomFrame)
-plaintext_entry.pack()
-
-plaintext_button = Button(bottomFrame, text="Encrypt", command=display_encrypt)
-plaintext_button.pack(pady=10)
-
-plaintext_label1 = Label(bottomFrame1, text="Encypted > Plaintext")
-plaintext_label1.pack(pady=10)
-
-plaintext_entry1 = Text(bottomFrame1)
-plaintext_entry1.pack()
-
-plaintext_button1 = Button(bottomFrame1, text="Decrypt", command=display_decrypt)
-plaintext_button1.pack(pady=10)
-
-third_frame = Frame(root, bg="grey")
+clear_button = Button(button_frame1, text="Clear All", command=clear_input)
+clear_button.pack(pady=10)
 
 root.mainloop()
 
 # end of GUI
 ##########################################################################
+
+# insert code for frequency analysis over here
